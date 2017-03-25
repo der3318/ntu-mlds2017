dca7c2ece46fd1f160a4d5e9eb22d211a4e72df3
der <b03902015@ntu.edu.tw>
2017-03-25 21:19:33 +0800

    finish deployment

--- hw1/run.sh
+++ hw1/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# mkdir
-mkdir -p model
-
 # download model from somewhere
+wget http://www.csie.ntu.edu.tw/~b03902007/model.data-00000-of-00001 -P model
 
 # output result to file
 python lstm.py $1 $2 --load
