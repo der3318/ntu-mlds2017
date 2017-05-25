56251ccf59d664c5728c45b5ffcdcfc05cbcdc7e
Wariard <r88093608@hotmail.com>
2017-05-25 17:14:20 +0800

    mkdir add -p

--- hw3/run.sh
+++ hw3/run.sh
@@ -1,6 +1,6 @@
 python3 preprocessing.py --test $1
-mkdir ./samples
-mkdir ./model
+mkdir -p ./samples
+mkdir -p ./model
 wget -P ./model/ https://www.dropbox.com/s/d2fv4j29pp3rhlp/model_after_epoch_598_dcgan.ckpt.data-00000-of-00001
 wget -P ./model/ https://www.dropbox.com/s/pyt6ydnhbhvhxtn/model_after_epoch_598_dcgan.ckpt.index
 wget -P ./model/ https://www.dropbox.com/s/ynf01d09equ8frx/model_after_epoch_598_dcgan.ckpt.meta
